f9df6c670be2dfa28419bfec490faa4446b33951
Rana Huzaima <huzaimadigital@gmail.com>
2023-09-28 12:48:03 +0500

    make button and input box

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import './index.css'
 function App() {
+  function gernator(){
+    let pass = "";
+    let str = "abcdefghijklmnopqrstuvwxyz";
+    let char = "!@#$%^&*()_+{}"
+    let num = "0123456789"
 
+  }
 
   return (
     <div  className="div">
-    <input type="text" readOnly placeholder="Password"/>
+    <input className='input' type="text" readOnly placeholder="Password"/>
     <div className="div-2">
     <input type="range" min="1" max="99" /><label>6</label>
-    <input type="checkbox" name="" id="" /><label>Number</label>
-    <input type="checkbox" name="" id="" /><label>characters</label>
+    <input type="checkbox" /><label>Number</label>
+    <input type="checkbox" /><label>characters</label>
 </div>
+    <button>Copy</button>
 </div>
   )
 }
