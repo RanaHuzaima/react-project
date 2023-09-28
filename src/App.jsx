1fe07c7bc9fe4de8bcf25fd33c936e16a48f25f5
Rana Huzaima <huzaimadigital@gmail.com>
2023-09-28 14:18:05 +0500

    fix Bug related to password generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import { useState, useCallback, useEffect, useRef } from 'react';
 import './index.css'
 function App() {
-  const [password , setpassword] = useState("")
+  const [password , setPassword] = useState("")
   const [length , setLength] = useState(8)
   const [numAllow , setnumAllow] = useState(false)
-  const [char , setChar] = useState(false)
-  function gernator(){
+  const [charAllow , setCharAllow] = useState(false)
+  const gernator = useCallback(()=>{
     let pass = "";
-    let str = "abcdefghijklmnopqrstuvwxyz";
-    let char = "!@#$%^&*()_+{}"
-    let num = "0123456789"
-  }
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numAllow) str += "0123456789";
+    if(charAllow) str += "!@#$%^&*()_+{}";
+
+    for (let i = 1; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
+    setPassword(pass)
+  }, [length,numAllow,charAllow,setPassword])
 
   return (
     <div  className="div">
       <h1>Password Generator</h1>
-    <input className='input' type="text" readOnly placeholder="Password"/>
+    <input className='input' value={password} type="text" readOnly placeholder="Password"/>
     <div className="div-2">
-    <input type="range" min="1" max="99" /><label>{length}</label>
-    <input type="checkbox" /><label>Number</label>
+    <input type="range" min="8" max="99" value={length} onChange={(e)=>{ setLength(e.target.value)}} /><label>{length}</label>
+    <input type="checkbox" defaultChecked={numAllow} onChange={()=>{ setnumAllow((prev)=> !prev)}} /><label>Number</label>
     <input type="checkbox" /><label>characters</label>
 </div>
     <button>Copy</button>
