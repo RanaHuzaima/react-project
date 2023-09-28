eeb32ce784f7b6438cf15fe5b50d028a24215aa0
Rana Huzaima <huzaimadigital@gmail.com>
2023-09-28 14:51:46 +0500

    Adding Comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState, useCallback, useEffect, useRef } from 'react';
 import './index.css'
 function App() {
+  //Declare State
   const [password , setPassword] = useState("")
   const [length , setLength] = useState(8)
   const [numAllow , setnumAllow] = useState(false)
   const [charAllow , setCharAllow] = useState(false)
-  const gernator = useCallback(()=>{
+  // UseRef Hook
+  // Make PassGernator function
+  const passGernator = useCallback(()=>{
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if(numAllow) str += "0123456789";
@@ -17,18 +20,19 @@ function App() {
     }
     setPassword(pass)
   }, [length,numAllow,charAllow,setPassword]);
-  useEffect(function on(){
-    gernator();
-  },[length,numAllow,charAllow,setPassword]);
 
+  // UseEffect Hook for call
+  useEffect(()=>{
+    passGernator();
+  },[length,numAllow,charAllow,passGernator]);
   return (
     <div  className="div">
-      <h1>Password Generator</h1>
+      <h1>Password Generator App</h1>
     <input className='input' value={password} type="text" readOnly placeholder="Password"/>
     <div className="div-2">
     <input type="range" min="8" max="99" value={length} onChange={(e)=>{ setLength(e.target.value)}} /><label>{length}</label>
     <input type="checkbox" id="numInput" defaultChecked={numAllow} onChange={()=>{ setnumAllow((prev)=> !prev)}} /><label htmlFor="numInput">Number</label>
-    <input type="checkbox" defaultChecked={charAllow} onChange={()=>{setCharAllow ((prev)=>!prev)}} /><label>characters</label>
+    <input type="checkbox" id="charInput" defaultChecked={charAllow} onChange={()=>{setCharAllow ((prev)=>!prev)}} /><label htmlFor='charInput'>characters</label>
 </div>
     <button>Copy</button>
 </div>
