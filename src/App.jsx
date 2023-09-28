4b506aa41f4d3bd3bcdf67602c565fd973d10dae
Rana Huzaima <huzaimadigital@gmail.com>
2023-09-28 13:00:02 +0500

    make state

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
+import { useState } from 'react';
 import './index.css'
+import { useRef } from 'react';
+import { useCallback } from 'react';
+import { useEffect } from 'react';
 function App() {
+  const [password , setpassword] = useState("")
+  const [length , setLength] = useState("8")
+  const [number , setNumber] = useState("")
+  const [char , setChar] = useState("")
   function gernator(){
     let pass = "";
     let str = "abcdefghijklmnopqrstuvwxyz";
     let char = "!@#$%^&*()_+{}"
     let num = "0123456789"
-
   }
 
   return (
     <div  className="div">
     <input className='input' type="text" readOnly placeholder="Password"/>
     <div className="div-2">
-    <input type="range" min="1" max="99" /><label>6</label>
+    <input type="range" min="1" max="99" /><label>{length}</label>
     <input type="checkbox" /><label>Number</label>
     <input type="checkbox" /><label>characters</label>
 </div>
