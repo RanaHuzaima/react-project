6783f054a9ca8604948346d885b652a2ff2fd48a
Rana Huzaima <huzaimadigital@gmail.com>
2023-09-28 12:11:17 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+function App() {
+
+
+  return (
+    <div  className="div">
+    <input type="text" readOnly placeholder="Password"/>
+    <div className="div-2">
+    <input type="range" min="1" max="99" /><label>6</label>
+    <input type="checkbox" name="" id="" /><label>Number</label>
+    <input type="checkbox" name="" id="" /><label>characters</label>
+</div>
+</div>
+  )
+}
+
+export default App
