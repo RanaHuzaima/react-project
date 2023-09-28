1a396b5efea01bed71be2ff72a13b80da264b385
Rana Huzaima <huzaimadigital@gmail.com>
2023-09-28 18:16:17 +0500

    add more customize features

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useCallback, useEffect, useRef } from "react";
+import {AiFillCopy,AiOutlineReload}  from "react-icons/ai";
 import "./index.css";
 function App() {
   //Declare State
@@ -6,12 +7,16 @@ function App() {
   const [length, setLength] = useState(8);
   const [numAllow, setnumAllow] = useState(false);
   const [charAllow, setCharAllow] = useState(false);
+  const [lowcase, setLowcase] = useState(false);
+  const [uppcase, setUppcase] = useState(false);
   // UseRef Hook
   const passwordRef = useRef(null);
   // Make PassGernator function
   const passGernator = useCallback(() => {
     let pass = "";
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let str = "";
+    if(uppcase) str += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if(lowcase) str += "abcdefghijklmnopqrstuvwxyz";
     if (numAllow) str += "0123456789";
     if (charAllow) str += "!@#$%^&*()_+{}";
 
@@ -20,7 +25,7 @@ function App() {
       pass += str.charAt(char);
     }
     setPassword(pass);
-  }, [length, numAllow, charAllow, setPassword]);
+  }, [length, numAllow, charAllow, setPassword,lowcase,uppcase]);
   // Make Copy Function
   const copyPassword = useCallback(() => {
     passwordRef.current?.select();
@@ -30,18 +35,19 @@ function App() {
   // UseEffect Hook for call
   useEffect(() => {
     passGernator();
-  }, [length, numAllow, charAllow, passGernator]);
+  }, [length, numAllow, charAllow, passGernator,lowcase,uppcase]);
   return (
     <div className="div">
       <h1>Password Generator App</h1>
       <input
         className="input"
-        value={password}
+        value={password?password:"Password"}
         type="text"
         readOnly
         ref={passwordRef}
         placeholder="Password"
-      />
+      /><i className="copy_logo" onClick={copyPassword} ><AiFillCopy/></i>
+       <i className="reload_logo" onClick={passGernator} ><AiOutlineReload/></i>
       <div className="div-2">
         <input
           type="range"
@@ -71,6 +77,22 @@ function App() {
           }}
         />
         <label htmlFor="charInput">characters</label>
+        <input 
+        type="checkbox" 
+        id="lowcase" 
+        defaultChecked={lowcase}
+        onChange={()=>{
+          setLowcase((prev) => !prev)
+        }}
+        />
+        <label htmlFor="lowcase">Lowercase</label>
+        <input 
+        type="checkbox"
+        id="uppcase"
+        defaultChecked={uppcase}
+        onChange={()=>{setUppcase((prev) => !prev)}}
+         />
+         <label htmlFor="uppcase">Uppercase</label>
       </div>
       <button onClick={copyPassword}>Copy</button>
     </div>
